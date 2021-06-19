a6e79944a0dc187068f1a98b8d355caa798f7f65
satoru-koyama <satoru123.fly.9.26@gmail.com>
2021-06-19 07:22:37 +0000

    [Finish] 4-9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    post_images = PostImage.where(user_id: @user.id)
-    @post_images = post_images.page(params[:page]).reverse_order
+    @post_images = @user.post_images.page(params[:page]).reverse_order
   end
 
   def edit
